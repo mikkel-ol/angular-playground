07cc2bd2349ae3bd5a22daac622913933ad6e09c
Mikkel O. Laursen <modl@seges.dk>
2023-01-30 17:53:54 +0100

    Add modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from "@angular/core"
-import { MaterialModule } from "./material/material.module"
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MaterialModule } from "./material/material.module";
+import { ListOptionComponent } from "./selection-list/list-option/list-option.component";
+import { SelectionListComponent } from "./selection-list/selection-list.component";
 
-const modules = [MaterialModule]
+const modules = [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule];
+
+const components = [SelectionListComponent, ListOptionComponent];
 
 @NgModule({
   imports: [...modules],
-  exports: [...modules],
+  exports: [...modules, ...components],
+  declarations: [...components],
 })
 export class SharedModule {}
