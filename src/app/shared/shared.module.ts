4bde7395572fcf403bcb4f3983eb18ad770197fb
Mikkel O. Laursen <modl@seges.dk>
2023-02-22 14:01:01 +0100

    Fix shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,10 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MaterialModule } from "./material/material.module";
-import { ListOptionComponent } from "./selection-list/list-option/list-option.component";
-import { SelectionListComponent } from "./selection-list/selection-list.component";
 
 const modules = [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule];
 
-const components = [SelectionListComponent, ListOptionComponent];
+const components: any[] = [];
 
 @NgModule({
   imports: [...modules],
