5c0c44b76808daacf04c9d9da4114a6a3bfafed1
Mikkel O. Laursen <miu@bankdata.dk>
2024-04-15 13:51:54 +0200

    Update to standalone application

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { enableProdMode } from "@angular/core";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-
-import { AppModule } from "./app/app.module";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
+import { config } from "./app/app.config";
 import { environment } from "./environments/environment";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, config).catch((err) => console.error(err));
