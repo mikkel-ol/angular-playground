615c818700affc7b88fa5d04d48f3cae162668f8
Mikkel O. Laursen <miu@bankdata.dk>
2024-07-23 13:37:44 +0200

    Provide animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig } from "@angular/core";
+import { provideAnimations } from "@angular/platform-browser/animations";
 import { provideRouter } from "@angular/router";
 
 export const config: ApplicationConfig = {
-  providers: [provideRouter([])],
+  providers: [provideRouter([]), provideAnimations()],
 };
